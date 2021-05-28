e1f42ef4d88f652b8a98ca72be63bf0509e55cdf
network <64174146+networkstuff@users.noreply.github.com>
2021-05-28 08:12:09 -0300

    Adding new information

---
+++ python/escapechar.py
@@ -0,0 +1,22 @@
+splitString = "this string has been\nsplt over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "no, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"no, no, 'e's uh,...he's resting\".")
+
+print("""The pet shop owner said "No, no, 'e's uh,..he's resting". """)
+
+anotherSplitString = """This string has been \
+split \
+several \
+lines"""
+
+print(anotherSplitString)
+#########
+
+print("Number   1\tThe Larch")
+print("Number   2\tThe Horse Chestnut")
---
+++ python/strings.py
@@ -0,0 +1,12 @@
+print("today is a 'good day' to learn python")
+print('python is fun')
+print("python's string are easy to use")
+print('we can even include "quotes" in strings')
+print("hello" + " world")
+###
+greeting = "hello"
+name = input("please enter your name")
+print(greeting + name)
+
+#if we a space, we can add that too
+print(greeting + ' ' + name)
