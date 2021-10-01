3f8b57e2eb1ae6ba65f5c931a246299dd2989608
networkstuff <64174146+networkstuff@users.noreply.github.com>
2021-10-01 17:20:05 -0300

    update

--- python/Old enough to ride with photo taken.py
+++ python/Old enough to ride with photo taken.py
@@ -9,6 +9,8 @@ if height >= 120:
     elif age <= 18:
         bill = 7
         print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Ride is free")
     else:
         bill = 12
         print("Adult tickets are $12")
