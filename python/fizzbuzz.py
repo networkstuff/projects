95dca10f752c1f4624aabc1fb160c2c5a0ba13b6
networkstuff <64174146+networkstuff@users.noreply.github.com>
2021-10-18 10:42:27 -0300

    update

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,3 +1,9 @@
+### Author: Ivan V
+### purpose: for educational/presentation purposes ONLY!
+### explanation: The challenge is to check if the number from 1 to 100 divided by a number (3 or 5) is equal or not. 
+#if the number is equal (meaning no left over) than it will print fizbuzz, fizz or buzz depending on the outcome.
+# if all is false than the outcome is the number is printed meaning the number divided by 3 or 5 is not an even number.
+
 for number in range(1, 101):
 
     if (number % 3) == 0 and number % 5 == 0:
